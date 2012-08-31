6765d79bfdfbf0a55fdae34e63abb92723eeca5b
Ubuntu <ubuntu@ip-10-58-150-82.eu-west-1.compute.internal>
2012-08-31 14:04:40 +0000

    now with working statsd!

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import traceback
 import sys
 import bcrypt
 import logging
+import statsd
 
 logger = logging.getLogger('baas')
 hdlr = logging.FileHandler('/var/log/baas.log')
@@ -11,11 +12,13 @@ formatter =  logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
+statsd_connection = statsd.Connection(host='localhost')
 
 app = Flask(__name__)
 
 @app.route('/')
 def slash():
+	statsd.Counter('baas',statsd_connection).increment('slash',delta=1)
 	return jsonify(status="ready"),200
 #	return '{"status":"ready"}',200
 
@@ -23,12 +26,14 @@ def slash():
 def crypt(text):
 	try:
 		hashed = bcrypt.hashpw(text,bcrypt.gensalt())
+		statsd.Counter('baas',statsd_connection).increment('crypt')
 		return jsonify(hash=hashed),200
 #		return '{"hash":"%s"}'%hashed,200		
 	except Exception as e:
 		err = traceback.format_exception(*sys.exc_info())
 		logger.info(err)
 #		return u'{"status":"NULL", "reason": "Probably an invalid salt"}' % err,400
+		statsd.Counter('baas',statsd_connection).increment('error.crypt',delta=1)
 		return jsonify(status="NULL",reason="Probably an invalid salt"),400
 
 
@@ -37,14 +42,17 @@ def ccrypt(text,complexity):
 	icomplexity = int(complexity)
 	if icomplexity >= 20:
 #		return u'{"status":"NULL", "reason": "Huge complexities take an impossible time to calculate. Get real."}',400
+		statsd.Counter('baas',statsd_connection).increment('error.ccrypt',delta=1)
 		return jsonify(status="NULL", reason="Huge complexities take an impossible time to calculate. Get real."),400
 	else:
 		try:
 			hashed = bcrypt.hashpw(text,bcrypt.gensalt(icomplexity))
+			statsd.Counter('baas',statsd_connection).increment('ccrypt',delta=1)
 			return jsonify(hash=hashed),200
 		except Exception as e:
 			err = traceback.format_exception(*sys.exc_info())
 			logger.info(err)
+			statsd.Counter('baas',statsd_connection).increment('error.ccrypt',delta=1)
 			return jsonify(status="NULL",reason="Probably an invalid complexity"),400
 
 @app.route('/scrypt/<text>/<salt>')
@@ -55,19 +63,23 @@ def scrypt(text,salt):
 		err = traceback.format_exception(*sys.exc_info())
 		logger.info(err)
 		#return u'{"status":"NULL", "reason": "Probably an invalid salt"}' % err,400
+		statsd.Counter('baas',statsd_connection).increment('error.scrypt',delta=1)
 		return jsonify(status="NULL", reason="Probably an invalid salt"),400
 
 	#return '{"hash":"%s"}'%hashed
+	statsd.Counter('baas',statsd_connection).increment('scrypt',delta=1)
 	return jsonify(hash=hashed),200
 
 @app.route('/gensalt')
 def gensalt():
 	try:
 		salt = bcrypt.gensalt()
+		statsd.Counter('baas',statsd_connection).increment('gensalt',delta=1)
 		return jsonify(salt=salt),200
 	except Exception as e:
 		err = traceback.format_exception(*sys.exc_info())
 		logger.info(err)
+		statsd.Counter('baas',statsd_connection).increment('error.gensalt',delta=1)
 		return jsonify(status="NULL", reason="Failed to generate salt"),500
 
 @app.route('/gensalt/<complexity>')
@@ -75,10 +87,12 @@ def cgensalt(complexity):
 	try:
 		ic = int(complexity)
 		salt = bcrypt.gensalt(ic)
+		statsd.Counter('baas',statsd_connection).increment('cgensalt',delta=1)
 		return jsonify(salt=salt),200
 	except Exception as e:
 		err = traceback.format_exception(*sys.exc_info())
 		logger.info(err)
+		statsd.Counter('baas',statsd_connection).increment('error.gensalt',delta=1)
 		return jsonify(status="NULL", reason="Failed to generate salt, value %s not valid"%complexity),500
 
 
